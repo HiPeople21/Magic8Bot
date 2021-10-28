69e4beee54f867ff9552a90b206a800204d0523e
HiPeople21 <86291833+HiPeople21@users.noreply.github.com>
2021-10-28 12:33:44 +0000

    Added slash command

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.14
 
-require github.com/bwmarrin/discordgo v0.23.2
+require github.com/bwmarrin/discordgo v0.23.3-0.20211010150959-f0b7e81468f7
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
   "encoding/json"
   "net/http"
   "log"
+  "flag"
 
   "github.com/bwmarrin/discordgo"
 )
@@ -22,16 +23,73 @@ type Response struct {
   Type string `json:"type"`
 }
 
+var (
+	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+)
+func init() { flag.Parse() }
 var BotID string
+var commands = []*discordgo.ApplicationCommand{
+  {
+    Name: "magic8",
+    Description: "Command for getting a magic 8 ball response",
+    Options: []*discordgo.ApplicationCommandOption{
+      {
+        Type: discordgo.ApplicationCommandOptionString,
+        Name: "question",
+        Description: "question",
+        Required: true,
+      },
+    },
+  },
+}
+
+var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+  "magic8": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+      channel := make(chan ResponseMagic)
+      go GetResponse(i.ApplicationCommandData().Options[0].StringValue(), channel)
+      response := <-channel
+      embed := &discordgo.MessageEmbed{
+        Title: response.Response.Answer,
+        Fields: []*discordgo.MessageEmbedField{
+          &discordgo.MessageEmbedField{
+            Name: "Question",
+            Value: response.Response.Question,
+          },
+          &discordgo.MessageEmbedField{
+            Name: "Type",
+            Value: response.Response.Type,
+          },
+        },
+      }
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Embeds: []*discordgo.MessageEmbed{
+            embed,
+          },
+				},
+			})
+		},
+}
+
+var discord *discordgo.Session
+
+func init() {
+  var err error
+  token := os.Getenv("token")
+	discord, err = discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+  discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
 
 func main() {
-  token := os.Getenv("token")
-  discord, err := discordgo.New("Bot " + token)
-  
-  if err != nil {
-    fmt.Println(err.Error())
-    return
-  }
 
   user, err := discord.User("@me")
 
@@ -41,17 +99,26 @@ func main() {
 
   BotID = user.ID
 
+  discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Println("Bot is up!")
+	})
   discord.AddHandler(MessageHandler)
-
+  
   err = discord.Open()
+
+  for _, v := range commands {
+		_, err := discord.ApplicationCommandCreate(discord.State.User.ID, "", v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+	}
 
   if err != nil {
     fmt.Println(err.Error())
     return
   }
 
-  fmt.Println("Bot is running!")
-
+  defer discord.Close()
   <- make(chan struct{})
   return
 }
@@ -66,9 +133,16 @@ func MessageHandler(session *discordgo.Session, message *discordgo.MessageCreate
     question := strings.TrimSpace(strings.TrimPrefix(message.Content, "!magic8"))
     if question == ""{
       embed := &discordgo.MessageEmbed{
-        Title: message.Author.Username +", question cannot be nothing.",
-        Description:"How to use command: '!magic8 (question)'",
-        }
+        Title: "Question cannot be nothing.",
+        Description:"How to use command: `!magic8 (question)`",
+        Fields: []*discordgo.MessageEmbedField{
+        &discordgo.MessageEmbedField{
+          Name: "Asker",
+          Value: message.Author.Mention(),
+        },
+        },
+      }
+    
       session.ChannelMessageSendEmbed(message.ChannelID, embed)
       return
     }
@@ -86,6 +160,10 @@ func MessageHandler(session *discordgo.Session, message *discordgo.MessageCreate
           Name: "Type",
           Value: response.Response.Type,
         },
+        &discordgo.MessageEmbedField{
+          Name: "Asker",
+          Value: message.Author.Mention(),
+        },
       },
     }
 
@@ -93,8 +171,17 @@ func MessageHandler(session *discordgo.Session, message *discordgo.MessageCreate
   }
 }
 
+func HandlePanic() {
+  if r := recover(); r!= nil {
+        fmt.Println("recovered from ", r)
+  }
+}
+
 
 func GetResponse(question string, channel chan ResponseMagic) {
+  defer HandlePanic()
+  
+  
   res, err := http.Get("https://8ball.delegator.com/magic/JSON/" + question)
   if err != nil {
     channel<-ResponseMagic{
@@ -120,7 +207,13 @@ func GetResponse(question string, channel chan ResponseMagic) {
   var responseObject ResponseMagic
   jsonErr := json.Unmarshal(responseData, &responseObject)
   if jsonErr != nil {
-		log.Fatal(jsonErr)
+		channel<-ResponseMagic{
+      Response: Response{
+        Question: question,
+        Answer: "Error. Cannot accept questions with '/' or a single backslash.",
+        Type: "N/A",
+      },
+    }
 	}
   channel<-responseObject
 }
